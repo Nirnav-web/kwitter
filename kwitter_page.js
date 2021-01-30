9d7d2b6aa682000fbbe04592e9f92b75412f1577
Nirnav-web <76113884+Nirnav-web@users.noreply.github.com>
2021-01-30 12:13:04 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,19 +1,31 @@
 var firebaseConfig = {
-      apiKey: "AIzaSyCl52mVozu_WyIPB05CRD8Mr84O7t-5I7I",
-      authDomain: "kwitter-fc8c2.firebaseapp.com",
-      databaseURL: "https://kwitter-fc8c2-default-rtdb.firebaseio.com",
-      projectId: "kwitter-fc8c2",
-      storageBucket: "kwitter-fc8c2.appspot.com",
-      messagingSenderId: "744453561097",
-      appId: "1:744453561097:web:bbb5e00426da17604a51d8",
-      measurementId: "G-1NKMVW63WB"
+      apiKey: "AIzaSyBEyCYGVO-ZwC-JobgX-qsOYnOc3A99otc",
+      authDomain: "kwitter-f9727.firebaseapp.com",
+      projectId: "kwitter-f9727",
+      storageBucket: "kwitter-f9727.appspot.com",
+      messagingSenderId: "937896257323",
+      appId: "1:937896257323:web:61a1d3cd154dcaeb6b876e",
+      databaseURL="https://kwitter-f9727-default-rtdb.firebaseio.com"
     };
+    var database = firebase.database();
     firebase.initializeApp(firebaseConfig);
     user_name=localStorage.getItem("user_name");
     room_name=localStorage.getItem("room_name");
  function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
+         console.log(firebase_message_id);
+         console.log(message_data);
+         name=message_data['name'];
+         message=message_data['message'];
+         like=message_data['like'];
+         name_with_tag="<h4>"+name+"<img src='tick.png' class='user_tick'></h4>";
+         message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+         like_buton="<button class='btn btn-warning' id="+firebase_message_id+"value="+like+"onclick='updateLike(this.id)'";
+         span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like: "+like+"</span></button><hr>";
+         row=name_with_tag+message_with_tag+like_buton+span_with_tag;
+         document.getElementById("output").innerHTML=row;
+
       } });  }); }
 getData();
 function send(){
@@ -24,4 +36,18 @@ function send(){
             like:0
       });
       document.getElementById("msg").value=""
+}
+function updateLike(){
+      console.log("clicked on like button-"+message_id);
+      button_id=message_id;
+      likes=document.getElementById(button_id).value;
+      updated_likes=Number(likes)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updateLike()
+      })
+}
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html"
 }
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,13 @@
-
 var firebaseConfig = {
-      apiKey: "AIzaSyCl52mVozu_WyIPB05CRD8Mr84O7t-5I7I",
-      authDomain: "kwitter-fc8c2.firebaseapp.com",
-      projectId: "kwitter-fc8c2",
-      storageBucket: "kwitter-fc8c2.appspot.com",
-      messagingSenderId: "744453561097",
-      appId: "1:744453561097:web:bbb5e00426da17604a51d8",
-      measurementId: "G-1NKMVW63WB"
+      apiKey: "AIzaSyBEyCYGVO-ZwC-JobgX-qsOYnOc3A99otc",
+      authDomain: "kwitter-f9727.firebaseapp.com",
+      projectId: "kwitter-f9727",
+      storageBucket: "kwitter-f9727.appspot.com",
+      messagingSenderId: "937896257323",
+      appId: "1:937896257323:web:61a1d3cd154dcaeb6b876e",
+      databaseURL="https://kwitter-f9727-default-rtdb.firebaseio.com"
     };
+    var database = firebase.database();
     firebase.initializeApp(firebaseConfig);
     user_name=localStorage.getItem("user_name");
     document.getElementById("user_name").innerHTML="Welcome "+user_name+"!";
@@ -17,7 +17,7 @@ var firebaseConfig = {
           purpose:"add username"
     });
     localStorage.setItem("room_name",room_name);
-    window.location="kwitter_page.html"
+    window.location="kwitter_page.html";
     }
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
